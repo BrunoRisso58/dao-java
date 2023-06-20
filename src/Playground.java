1cb364630f1ffc92ebd7eb4a5a3e352238597e1a
Bruno Risso <brunorisso58@gmail.com>
2023-06-20 00:11:11 -0300

    Adicionando mais testes unitários

--- src/Playground.java
+++ src/Playground.java
@@ -13,6 +13,8 @@ public class Playground {
     private ContatoVO obj1 = new ContatoVO("Bruno", "bruno@email.com");
     private ContatoVO obj2 = new ContatoVO("Vitor", "vitor@email.com");
     private ContatoVO obj3 = new ContatoVO("Thainá", "thaina@email.com");
+    private ContatoVO obj4 = new ContatoVO("Esdras", "esdras@email.com");
+
 
     @BeforeEach
     public void setup() {
@@ -38,6 +40,38 @@ public class Playground {
         // Verificar se o contato foi encontrado corretamente
         Assertions.assertNotNull(contatoSalvo);
         Assertions.assertEquals(obj2.getNome(), contatoSalvo.getNome());
+    }
+
+    @Test
+    public void testAtualizarContato() throws Exception {
+        // Criar e salvar um contato de teste
+        service.salvar(obj3);
+
+        // Atualizar o nome do contato
+        obj3.setNome("Patrícia Silva");
+        service.atualizar(obj3);
+
+        // Buscar o contato atualizado
+        ContatoVO contatoAtualizado = service.buscarPorId(3);
+
+        // Verificar se o nome foi atualizado corretamente
+        Assertions.assertNotNull(contatoAtualizado);
+        Assertions.assertEquals(obj3.getNome(), contatoAtualizado.getNome());
+    }
+
+    @Test
+    public void testExcluirContato() throws Exception {
+        // Criar e salvar um contato de teste
+        service.salvar(obj4);
+
+        // Excluir o contato
+        service.excluir(4);
+
+        // Tentar buscar o contato excluído pelo id
+        ContatoVO contatoExcluido = service.buscarPorId(4);
+
+        // Verificar se o contato foi excluído corretamente
+        Assertions.assertNull(contatoExcluido);
     }
 
 }
